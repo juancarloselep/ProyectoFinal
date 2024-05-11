26448fd0980d201390b174e285187b7132dbabc1
JUAN CARLOS LÁZARO PÉREZ <juan.lazaro@ingenieria.unam.edu>
2024-05-10 20:51:17 -0600

    Agregué el zombie arrastrándose

--- ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -790,8 +790,9 @@ int main() {
 		// -------------------------------------------------------------------------------------------------------------------------
 		// Segundo Personaje Animacion
 		// -------------------------------------------------------------------------------------------------------------------------
-		modelOp = glm::translate(glm::mat4(1.0f), glm::vec3(-10.0f, -2.0f, 15.0f));
-		modelOp = glm::scale(modelOp, glm::vec3(0.0001f));
+		modelOp = glm::translate(glm::mat4(1.0f), glm::vec3(79.347f, -1.9f, -18.02f));
+		modelOp = glm::rotate(modelOp, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		modelOp = glm::scale(modelOp, glm::vec3(0.04f));
 		animShader.setMat4("model", modelOp);
 		zombieArrastre.Draw(animShader);
 
